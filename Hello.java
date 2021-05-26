77405e6767cf954087cb56bde271d6c97a3b7527
blquaye <71671036+blquaye@users.noreply.github.com>
2021-05-26 08:48:44 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*********************************************
+* lab01
+*********************************************
+* Brandall Quaye
+*05/26/2021
+*CMSC-255-C01
+**********************************************/
+public class Hello{
+	public static void main(String[]args){
+		System.out.println("Brandall Quaye");
+		System.out.println("Lab 01");
+		System.out.println("Run first java program");
+		System.out.println("05/26/2021");
+		System.out.println("CMSC-255-C01");
+		System.out.println("");
+	}
+}
